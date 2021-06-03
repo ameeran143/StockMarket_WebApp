3d5857a1b3bcffc7a71231f3dc57d2057f15faa9
Aashir Meeran <aashir.meeran@gmail.com>
2021-06-03 12:00:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import PIL as image
+
+# Adding a Title and image to the Webapp
+st.write("""
+# Stock Market WebApp 
+**Visually display data on a stock and a machine learning model to predict future prices.**
+""")
